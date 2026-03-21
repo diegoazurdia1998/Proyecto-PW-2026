d95c870fddf8d2893a3c0dad886e72a978ae315f
diegoazurdia1998 <98101214+diegoazurdia1998@users.noreply.github.com>
2026-03-21 00:09:13 -0600

    Modify Vite config to use react-swc and set base path
    Updated Vite configuration to use @vitejs/react-swc plugin and added base path for GitHub repository.

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -11,4 +12,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+})
+*/
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc' // o @vitejs/plugin-react
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Proyecto-PW-2026/', // DEBE ser el nombre exacto de tu repo en GitHub
 })
